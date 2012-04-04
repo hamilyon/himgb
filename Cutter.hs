a3791b81654fabc2a5a1e1ffc119a36dfc5f95d7
Alexander A Shaposhnikov <hamilyon@yandex.ru>
2012-04-04 08:52:02 +0400

    harness further work

--- Cutter.hs
+++ Cutter.hs
@@ -11,31 +11,41 @@ import Data.Maybe
 isSpamWithTolerance :: (SpamClassificationData -> [String] -> Double) -> (Double -> Bool) -> SpamClassificationData -> [String] -> Bool
 isSpamWithTolerance classificator tolerance trainingData message  = (tolerance . (classificator trainingData)) message
 
+
+
 tolerance :: Double -> Bool
 tolerance x = if x > 0.9 then True else False
 
-filterSpam :: HiperText -> (Stem -> String -> [String]) -> (SpamClassificationData -> [String] -> Double) -> SpamClassificationData -> Bool
-filterSpam = undefined
+filterSpam :: (Stem -> String -> [String]) -> (SpamClassificationData -> [String] -> Double) -> SpamClassificationData -> HiperText -> Bool
+filterSpam tokener classificator trainingData = (nothingToFalse . cutSpam tokener classificator trainingData)
 
-cutSpam :: HiperText -> (Stem -> String -> [String]) -> (SpamClassificationData -> [String] -> Double) -> SpamClassificationData -> Maybe Action
-cutSpam hiperText tokener classificator trainingData | (null . childs) hiperText            = Nothing
+nothingToFalse :: Maybe a -> Bool
+nothingToFalse Nothing = False
+nothingToFalse _ = True
+
+
+cutSpam :: (Stem -> String -> [String]) -> (SpamClassificationData -> [String] -> Double) -> SpamClassificationData -> HiperText -> Maybe Action
+cutSpam tokener classificator trainingData hiperText | (null . childs) hiperText            = Nothing
                                                      | (length . visibleText) hiperText < 5 = Nothing
                                                      | isSpam hiperText                     = Just (Cut hiperText)
                                                      | otherwise                            = 
-                                                            Just (Cut $ (fromChildren . catMaybes) {- $ map 
+                                                            Just (Cut $ (fromChildren . spamToCut) {- $ map 
                                                             (cutSpam' :: HiperText -> Maybe Action) -}
-                                                            (toMaybes
-                                                                (toChildren hiperText)))
+                                                            (toChildren hiperText))
         where
         visibleText = (tokener (Stem True) . plainify) :: HiperText -> [String]
         isSpam = (isSpamWithTolerance classificator tolerance trainingData) . visibleText
-        cutSpam' ht = cutSpam ht tokener classificator trainingData :: Maybe Action
+        cutSpam' = cutSpam
+        spamToCut = filter (filterSpam tokener classificator trainingData)
 
 fromChildren :: [HiperText] -> HiperText
 fromChildren children = HiperText (map ((,"")) children) Nothing
 
 toChildren :: HiperText -> [HiperText]
 toChildren hiperText = map fst (childs hiperText)
+
+fromAction :: Action -> HiperText
+fromAction (Cut hiperText) = hiperText
 
 toMaybes = map Just
 
--- HtmlSpam.hs
+++ HtmlSpam.hs
@@ -7,19 +7,21 @@ data HiperText =
     HiperText {
         childs :: [(HiperText, String)],
         effectiveAttr :: Maybe HiperTextAttr
-    }
+    } deriving Show
 
 data HiperTextAttr = 
     HiperTextAttr {
         style :: HiperTextStyle,
         href :: Maybe HiperTextHref
-    }
+    } deriving Show
 
 data HiperTextStyle = HiperTextStyle (String, String) -- key-value
+    deriving Show
 data HiperTextHref = HiperTextHref (String)
-
+    deriving Show
 data Stem = Stem {
     contextTags :: Bool
 }
 
-data Action = Cut (HiperText)
+data Action = Cut (HiperText) 
+    deriving Show
