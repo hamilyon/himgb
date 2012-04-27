199196afccfa8c4aec658d1e7134ee5c1a7ca338
Alexander A Shaposhnikov <hamilyon@yandex.ru>
2012-04-27 19:29:56 +0400

    slow prototype finished

--- CheckTrained.hs
+++ CheckTrained.hs
@@ -34,7 +34,9 @@ plus o1 o2 = Stats
 detectedSpamOkUnit      = Stats 0 0 1 0 0 0
 detectedHamOkUnit       = Stats 0 0 0 1 0 0
 detectedFalseSpamUnit   = Stats 0 0 0 0 1 0
-detectedFalseHamUnit     = Stats 0 0 0 0 0 1
+detectedFalseHamUnit    = Stats 0 0 0 0 0 1
+
+nullStats               = Stats 0 0 0 0 0 0
 
 accumulateStats ::  (String -> Bool) ->
                     (Stats) ->
@@ -42,14 +44,14 @@ accumulateStats ::  (String -> Bool) ->
                     [String] ->
                     Stats
 
---accumulateStats     clasifier
---                    good
---                    bad  = foldr (`plus` (getstats clasifier detectedHamOkUnit detecteFalseHamUnit))
+accumulateStats isSpam okUnit falseUnit checkMessages = foldr plus nullStats
+            (map
+                ((\x -> if x then okUnit else falseUnit) . isSpam)
+                checkMessages)
 
-accumulateStats = undefined
 
-getstats :: (String -> Bool) -> (Integer -> Stats) -> (Integer -> Stats) -> String -> Stats
-getstats = undefined
+-- getstats :: (String -> Bool) -> (Integer -> Stats) -> (Integer -> Stats) -> String -> Stats
+-- getstats = undefined
 
 batch_size :: Int
 batch_size = 10 :: Int
@@ -57,30 +59,18 @@ batch_size = 10 :: Int
 dir = "../corpi/test_data/"
 
 checkTrained = do
-    spamTrainBatch <- concat <$> readLorienBatch (dir ++ "spam/train/")
-    hamTrainBatch  <- concat <$> readBatch       (dir ++ "ham/train.txt")
-    spamTestBatch  <- readLorienBatch            (dir ++ "spam/test/")
-    hamTestBatch   <- readBatch                  (dir ++ "ham/test/")
+    spamTrainBatch <- concat <$> readLorienBatch (dir </> "spam/train/")
+    hamTrainBatch  <- readPlain       (dir </> "ham/train.txt")
+    spamTestBatch  <- readLorienBatch            (dir </> "spam/test/")
+    hamTestBatch   <- readPlain                  (dir </> "ham/test.txt")
 
     let trainedClasifier = train spamTrainBatch hamTrainBatch 1
-    printTestQuality trainedClasifier spamTestBatch hamTestBatch
-
--- readLorienBatch :: FilePath -> IO [String]
--- readLorienBatch path = do
---     files <- (getBatchofFiles path batch_size) :: IO [String]
---     lorien_files <- mapM ((fmap lorienToPlain) . readData) files
---     return lorien_files
+    printTestQuality trainedClasifier spamTestBatch (lines hamTestBatch)
 
 readLorienBatch path = readBatchofFilesSkipErrors path batch_size lorienToPlain
 
-readBatch :: FilePath -> IO [String]
-readBatch path = do
-    files <- (getBatchofFiles path batch_size) :: IO [String]
-    plain_files <- mapM readData files
-    return plain_files
-
-getBatchofFiles :: FilePath -> Int -> IO [String]
-getBatchofFiles path batch_size = (take batch_size) <$> getFiles path
+readPlain :: FilePath -> IO String
+readPlain = readDataSkipErrorsStrict
 
 getFiles :: FilePath -> IO [String]
 getFiles path = (map (path </>)) <$> (filter (not . isHidden)) <$> getDirectoryContents path
@@ -89,21 +79,7 @@ readBatchofFilesSkipErrors :: FilePath -> Int -> (String -> String) -> IO [Strin
 readBatchofFilesSkipErrors path batch_size processor = do
     files <- (getFiles path) :: IO [String]
     processed_files <- (fmap (map processor)) (readFilesMax files batch_size)
-
-    -- processed_files <- readFilesMax files batch_size
-    putStr $ "ok I go"
-    -- let filteredEmpty = filter (not . null) (files)
-    -- let batch = take batch_size filteredEmpty
     return (processed_files)
-    -- do
-    -- allFiles <- (getFiles path) :: IO [String]
-
-
-
-readDataEmptyOnError :: FilePath -> IO (String)
-readDataEmptyOnError = 
-    undefined
-    -- catch (readData) (\_ -> return "")
 
 isHidden ('.':_) = True
 isHidden _       = False
@@ -121,4 +97,3 @@ printTestQuality trainedClasifier spamTestBatch hamTestBatch = do
                             detectedSpamOkUnit
                             detectedFalseSpamUnit
                             spamTestBatch)
-
