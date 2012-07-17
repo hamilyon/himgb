55ecd09460b756296459ec31a2904eabee73d8de
Alexander A Shaposhnikov <hamilyon@yandex.ru>
2012-07-17 08:54:16 +0400

    added typical spam english vector and results

--- ProduceGuessEstimates.hs
+++ ProduceGuessEstimates.hs
@@ -9,28 +9,30 @@ import ReadString
 import System.FilePath ((</>))
 import Lorien
 import Control.Monad
+import qualified Data.Map as Map
 
-main = filesWithEnglishCorrelation (dir </> "spam/test/") 3000 0.3
+-- main = filesWithEnglishCorrelation (dir </> "spam/test/") 3000 0.3 typicalSpam
+main = checkEnglishCorrelation (dir </> "spam/test/") 3000 typicalSpam
 
 -- main = testCulprits
 
-checkEnglishCorrelation path max = do
+checkEnglishCorrelation path max etalon = do
     files <- (getFiles path) :: IO [String]
     spamTestBatch  <- readLorienListZip (take max files) :: IO [(FilePath, String)]
     
-    corr <- (mapM englishFequenciesCorrelation) spamTestBatch
-    -- foldr1 (>>) (map (putStrLn . show) corr)
-    return ()
+    corr <- (mapM (englishFequenciesCorrelation etalon)) spamTestBatch
+    foldr1 (>>) (map (putStrLn . show) corr)
+    --return ()
 
-filesWithEnglishCorrelation path max threshhold = do
-    filenames <- fmap (map fst) (filterByEnglishCorrelation path max threshhold)
+filesWithEnglishCorrelation path max threshhold etalon = do
+    filenames <- fmap (map fst) (filterByEnglishCorrelation path max threshhold etalon)
     mapM_ putStrLn filenames
 
-filterByEnglishCorrelation :: FilePath -> Int -> Double -> IO [(FilePath, Double)]
-filterByEnglishCorrelation path max threshhold  = do
+filterByEnglishCorrelation :: FilePath -> Int -> Double -> Map.Map Char Double -> IO [(FilePath, Double)]
+filterByEnglishCorrelation path max threshhold etalon = do
     files <- (getFiles path) :: IO [String]
     spamTestBatch  <- readLorienListZip (take max files) :: IO [(FilePath, String)]
-    freqs <- (mapM englishFequenciesCorrelation) spamTestBatch
+    freqs <- (mapM (englishFequenciesCorrelation etalon)) spamTestBatch
     return (filter ((>=threshhold) . snd) freqs)
     
 
@@ -38,8 +40,8 @@ filterByEnglishCorrelation path max threshhold  = do
 -- showEnglishFequenciesCorrelation inputString = do 
 --    putStrLn $ show $ englishFequenciesCorrelation inputString
 
-englishFequenciesCorrelation :: (FilePath, String) -> IO (FilePath, Double)
-englishFequenciesCorrelation inputString = do 
-    let result = correlation typicalEnglish ((freq . snd) inputString)
+englishFequenciesCorrelation :: Map.Map Char Double -> (FilePath, String) -> IO (FilePath, Double)
+englishFequenciesCorrelation etalon inputString = do 
+    let result = correlation etalon ((freq . snd) inputString)
     return (fst inputString, result)
 
---
+++ make.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#make.sh
+
+
+ghc -rtsopts=all -fforce-recomp Measure.hs
